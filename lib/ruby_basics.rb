af6ad557b313f93ceb58d3f9548626a6bd9c0f9b
John Sime <johnc.sime@gmail.com>
2018-10-01 21:51:38 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+ num1 / num2
 end
 
-def assign_variable(value)
+def assign_variable(Bob)
   
 end
 
-def argue
-  
+def argue(phrase)
+  puts phrase
 end
 
-def greeting
+def greeting(greeting, name)
+  puts "Hi there, #{name}"
 
 end
 
-def return_a_value
-
+def return_a_value(value = "Bob")
+  puts value
 end
 
 def last_evaluated_value
-
+  last_evaluated_value = "expert"
 end
 
-def pizza_party
-
+def pizza_party(phrase = "cheese")
+  puts phrase
 end
+
+pizza_party("pepperoni")
